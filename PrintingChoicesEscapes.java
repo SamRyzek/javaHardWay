048234859d72ca60c59d05bf69364d240ea6aa18
Samuel Ryzek <30350398+SamRyzek@users.noreply.github.com>
2017-07-28 15:39:27 -0600

    Delete PrintingChoicesEscapes.java

--- PrintingChoicesEscapes.java
+++
@@ -1,7 +0,0 @@
-public class PrintingChoicesEscapes
-{
-	public static void main( String[] args)
-	{
-		System.out.println( "Learn\tJava\n     the\nHard\tWay\n\n" + "\n\tLearn Java the \"Hard\" Way!\n" + "Hello\n" + "Jello\by\n" + "Hard to believe, eh?" + "Suprised? /* abcde */ Or what did you expect?" + "\\ // -=- \\ //" + "\\\\ \\\\\\ \\\\\\\\" + "I hope you understand \"escape sequences\" now.\n" );	
-	}
-}
