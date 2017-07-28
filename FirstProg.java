c650b0cca6c2eba9849bf9f181999128c389735e
Samuel Ryzek <30350398+SamRyzek@users.noreply.github.com>
2017-07-28 15:00:15 -0600

    Delete FirstProg.java

--- FirstProg.java
+++
@@ -1,8 +0,0 @@
-public class FirstProg 
-{
-	public static void main( String[] args )
-	{
-		System.out.println( "I, Samuel Ryzek, am determined to learn how to code." );
-		System.out.println( "Today's date is Wednesday, July 12, 2017." );
-	}
-}
