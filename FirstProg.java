e3bbf056871e3d4d29ee9205612d8c3afff767f1
Samuel Ryzek <samuelryzek@Samuels-MacBook-Pro.local>
2017-07-29 11:21:02 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg 
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "I, Samuel Ryzek, am determined to learn how to code." );
+		System.out.println( "Today's date is Wednesday, July 12, 2017." );
+	}
+}
